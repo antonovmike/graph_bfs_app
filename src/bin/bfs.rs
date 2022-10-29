b0b3b020b69fe5cb990bf27558f76aa4e341b154
antonovmike <antonvma@outlook.com>
2022-10-29 08:10:48 +0500

    lib test

--- src/bin/bfs.rs
+++ src/bin/bfs.rs
@@ -1,31 +1,6 @@
-// BREADTH FIRST SEARCH
+use graph_bfs_app::test;
 
-use std::collections::HashSet;
-
-pub struct Graph {
-    pub nodes: Vec<Node>,
-    pub edges: Vec<Edge>,
+fn main() {
+    let test = test(2, 2);
+    println!("TEST: {}", test);
 }
-
-pub struct Node(u32);
-pub struct Edge(u32, u32);
-
-impl Graph {
-    pub fn new(node: Vec<Node>, edge: Vec<Edge>) -> Self {
-        Graph { nodes: node, edges: edge }
-    }
-}
-
-pub fn breadth_first_search(graph: Graph, start_point: Node, end_point: Node, goal: Node) {
-    // Visited nodes?
-    let mut visited_nodes: HashSet<Node> = HashSet::new();
-    // visited_nodes insert?
-
-    // Neighbors?
-
-    // Reached goal
-
-    // Each Node checked, goal isn't met
-}
-
-fn main() {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,37 @@
-fn main() {
-    
+// TEST
+
+pub fn test(first: usize, second: usize) -> usize {
+    first + second
 }
+
+// BREADTH FIRST SEARCH
+
+use std::collections::HashSet;
+
+pub struct Graph {
+    pub nodes: Vec<Node>,
+    pub edges: Vec<Edge>,
+}
+
+pub struct Node(u32);
+pub struct Edge(u32, u32);
+
+impl Graph {
+    pub fn new(node: Vec<Node>, edge: Vec<Edge>) -> Self {
+        Graph { nodes: node, edges: edge }
+    }
+}
+
+pub fn breadth_first_search(graph: Graph, start_point: Node, end_point: Node, goal: Node) {
+    // Visited nodes?
+    let mut visited_nodes: HashSet<Node> = HashSet::new();
+    // visited_nodes insert?
+
+    // Neighbors?
+
+    // Reached goal
+
+    // Each Node checked, goal isn't met
+}
+
+fn main() {}
