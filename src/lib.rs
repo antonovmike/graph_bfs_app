ed8d9a01d79c9a15f5242bf8da459dd6f10074e2
antonovmike <antonvma@outlook.com>
2022-10-29 11:03:36 +0500

    add generic

--- src/lib.rs
+++ src/lib.rs
@@ -7,34 +7,34 @@ pub fn test(first: u8, second: u8) -> u8 {
 }
 
 // ADD AND REMOVE NODES
-pub fn add_node(graph: Graph, to_add: Node) {}
-pub fn rem_node(graph: Graph, to_remove: Node) {}
+pub fn add_node<N, E>(graph: Graph<N, E>, to_add: Node<N>) {}
+pub fn rem_node<N, E>(graph: Graph<N, E>, to_remove: Node<N>) {}
 
 // ADD AND REMOVE DIRECTED EDGES
-pub fn add_edge(graph: Graph, to_add: Edge) {}
-pub fn rem_edge(graph: Graph, to_remove: Edge) {}
+pub fn add_edge<N, E>(graph: Graph<N, E>, to_add: Node<E>) {}
+pub fn rem_edge<N, E>(graph: Graph<N, E>, to_remove: Node<E>) {}
 
 // SERDE INTO TRIVIAL GRAPH FORMAT
 
 // BREADTH FIRST SEARCH
 
-pub struct Graph {
-    pub nodes: Vec<Node>,
-    pub edges: Vec<Edge>,
+pub struct Graph<N, E> {
+    pub nodes: Node<N>,
+    pub edges: Edge<E>,
 }
 
-pub struct Node(u32);
-pub struct Edge(u32, u32);
+pub struct Node<N>(N);
+pub struct Edge<E>(E, E);
 
-impl Graph {
-    pub fn new(node: Vec<Node>, edge: Vec<Edge>) -> Self {
-        Graph { nodes: node, edges: edge }
+impl<N, E> Graph<N, E> {
+    pub fn new(nodes: Node<N>, edges: Edge<E>) -> Self {
+        Graph { nodes: nodes, edges: edges }
     }
 }
 
-pub fn breadth_first_search(graph: Graph, start_node: Node, end_node: Node, goal: Node) -> Vec<u32> {
+pub fn breadth_first_search<N, E>(graph: Graph<N, E>, start_node: Node<N>, end_node: Node<N>, goal: Node<N>) -> Vec<u32> {
     // Visited nodes?
-    let mut visited_nodes: HashSet<Node> = HashSet::new();
+    let mut visited_nodes: HashSet<Node<N>> = HashSet::new();
     // visited_nodes insert?
 
     // Neighbors?
