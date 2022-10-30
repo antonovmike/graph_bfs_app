499bd44e349b0ce812de65758b4de2667fc981f7
antonovmike <antonvma@outlook.com>
2022-10-30 08:08:23 +0500

    bfs dead end

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #![allow(unused)]
 use std::collections::HashSet;
 use std::collections::VecDeque;
+use core::hash::Hash;
+// use std::hash::Hash;
 
 // --> ADD AND REMOVE NODES
 
@@ -23,7 +25,7 @@ where
     new_vec
 }
 
-// --> ADD AND REMOVE DIRECTED EDGES
+// --> ADD AND REMOVE (not)DIRECTED EDGES
 
 pub fn add_edge<N, E>(graph: Graph<N, E>, to_add: E) -> Graph<N, E> {
     let mut new_vec = graph;
@@ -53,8 +55,9 @@ pub struct Graph<N, E> {
     pub edges: Vec<E>,
 }
 
-#[derive(Eq, Hash, PartialEq)]
+#[derive(Clone, Eq, Hash, PartialEq)]
 pub struct Node<N>(N);
+#[derive(Clone)]
 pub struct Edge<E>(E, E);
 
 impl<N, E> Graph<N, E> {
@@ -63,24 +66,29 @@ impl<N, E> Graph<N, E> {
     }
 }
 
-pub fn breadth_first_search<N, E>(graph: Graph<N, E>, start_node: N, end_node: N, goal: N) -> Vec<u32> {
+pub fn breadth_first_search<N, E>(
+    graph: Graph<N, E>, start_node: N, end_node: N, goal: N
+) -> String 
+where N: Eq, N: Hash, N: PartialEq
+{
     // Visited nodes?
-    // let mut visited_nodes: HashSet<N> = HashSet::new();
+    let mut visited_nodes: HashSet<N> = HashSet::new();
     let mut visited_nodes: Vec<bool> = vec![false; graph.nodes.len()];
     visited_nodes[0] = true;
     // visited_nodes insert?
-    let mut history: Vec<Node<N>> = Vec::new();
+    // let mut history: Vec<Node<N>> = Vec::new();
     // Sequence?
-    let mut queue: VecDeque<N> = VecDeque::new();
-    queue.push_back(start_node);
+    // let mut queue: VecDeque<N> = VecDeque::new();
+    // queue.push_back(start_node);
 
     // Neighbors?
+    // let mut previous: Vec<Option<N>> = vec![None; graph.nodes.len()];
 
     // Reached goal
 
     // Each Node checked, goal isn't met
 
-    vec![1, 2, 3]
+    "a".to_string()
 }
 
 fn main() {}
