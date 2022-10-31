6b5d95e962f356613687a8b363b83781e1dd9fcd
antonovmike <antonvma@outlook.com>
2022-10-31 16:26:25 +0500

    bfs works in a wrong way

--- src/bin/bfs.rs
+++ src/bin/bfs.rs
@@ -3,28 +3,35 @@ use graph_bfs_app::rem_node;
 use graph_bfs_app::add_node;
 use graph_bfs_app::rem_edge;
 use graph_bfs_app::add_edge;
+use graph_bfs_app::bfs;
+use graph_bfs_app::{Node, Edge};
 
 fn main() {
     // --> CREATE NEW GRAPH
-    let some_nodes: Vec<u8> = vec![1, 2, 3, 4];
-    let some_edges: Vec<(u8, u8)> = vec![(1, 2), (3, 4)];
-    let gr_0: Graph<u8, (u8, u8)> = Graph::new(some_nodes, some_edges);
+    let some_nodes: Vec<Node> = vec![Node(1), Node(2), Node(3), Node(4)];
+    let some_edges: Vec<Edge> = vec![Edge(1, 2), Edge(3, 4)];
+    let gr_0 = Graph::new(some_nodes, some_edges);
     println!("Graph new nodes: \t{:?}", gr_0.nodes);
     println!("Graph new edges: \t{:?}", gr_0.edges);
 
     // --> ADD AND REMOVE NODES
-    let gr_1: Graph<u8, (u8, u8)> = add_node(gr_0, 7);
+    let seven = Node(7);
+    let gr_1 = add_node(gr_0, seven);
     println!("One node added: \t{:?}", gr_1.nodes);
-    let gr_2: Graph<u8, (u8, u8)> = rem_node(gr_1, 7);
+    let gr_2 = rem_node(gr_1, Node(7));
     println!("One node removed: \t{:?}", gr_2.nodes);
 
     // --> ADD AND REMOVE DIRECTED EDGES
-    let gr_3: Graph<u8, (u8, u8)> = add_edge(gr_2, (2, 3));
+    let two_three = Edge(2, 3);
+    let gr_3 = add_edge(gr_2, two_three.clone());
     println!("One edge added: \t{:?}", gr_3.edges);
-    let gr_4: Graph<u8, (u8, u8)> = rem_edge(gr_3, (2, 3));
+    let gr_4 = rem_edge(gr_3, two_three);
     println!("One edge added: \t{:?}", gr_4.edges);
 
     // SERDE INTO TRIVIAL GRAPH FORMAT
 
     // BREADTH FIRST SEARCH
+    let gr_vector: Vec<Vec<i32>> = vec![vec![1, 2], vec![3, 4], vec![2, 4]];
+    let found = bfs(gr_vector, 0, 2);
+    println!("BFS: \t\t\t{:?}", found.as_ref().unwrap());
 }
