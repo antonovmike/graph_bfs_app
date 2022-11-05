a8eb9654073a8f6cd86caab79e67be763cdeab25
antonovmike <antonvma@outlook.com>
2022-11-05 09:29:22 +0500

    generic Node ok, fix bfs, add serde

--- src/bin/bfs.rs
+++ src/bin/bfs.rs
@@ -8,7 +8,7 @@ use graph_bfs_app::{Node, Edge};
 
 fn main() {
     // --> CREATE NEW GRAPH
-    let some_nodes: Vec<Node> = vec![Node(1), Node(2), Node(3), Node(4)];
+    let some_nodes: Vec<Node<i32>> = vec![Node(1), Node(2), Node(3), Node(4)];
     let some_edges: Vec<Edge> = vec![Edge(1, 2), Edge(3, 4)];
     let gr_0 = Graph::new(some_nodes, some_edges);
     println!("Graph new nodes: \t{:?}", gr_0.nodes);
@@ -23,17 +23,19 @@ fn main() {
 
     // --> ADD AND REMOVE DIRECTED EDGES
     let two_three = Edge(2, 3);
+    // let three_two = Edge(3, 2);
     let gr_3 = add_edge(gr_2, two_three.clone());
     println!("One edge added: \t{:?}", gr_3.edges);
     let gr_4 = rem_edge(gr_3, two_three);
-    println!("One edge added: \t{:?}", gr_4.edges);
+    println!("One edge removed: \t{:?}", gr_4.edges);
 
-    // SERDE INTO TRIVIAL GRAPH FORMAT
+    let gr_5 = add_edge(gr_4, two_three.clone());
 
-    // BREADTH FIRST SEARCH
-    // let gr_vector: Vec<Vec<i32>> = vec![vec![1, 2], vec![3, 4], vec![2, 4]];
-    // let found = bfs(gr_vector, 0, 2);
-    let found = bfs(&gr_4, Node(1), Node(2));
+    // --> SERDE INTO TRIVIAL GRAPH FORMAT
+    // ...
+
+    // --> BREADTH FIRST SEARCH
+    let found = bfs(&gr_5, Node(0), Node(2));
     // println!("BFS: \t\t\t{:?}", found.as_ref().unwrap());
     println!("BFS: \t\t\t{:?}", found);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,39 @@
 #![allow(unused)]
-use std::collections::{HashSet, VecDeque};
-
-type NodeType = Vec<i32>;
-type GraphType = Vec<NodeType>;
-struct Queue<T> {
-    pub items: VecDeque<T>,
-}
-impl<T> Queue<T> {
-    pub fn new() -> Queue<T> {
-        Queue {
-            items: VecDeque::new(),
-        }
-    }
-
-    pub fn enqueue(&mut self, v: T) {
-        self.items.push_back(v)
-    }
-
-    pub fn dequeue(&mut self) -> T {
-        self.items
-            .pop_front()
-            .expect("Cannot dequeue from empty queue.")
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.items.len() == 0
-    }
-}
+use std::{collections::{HashSet, VecDeque}, hash::Hash};
 
 #[derive(Clone)]
-pub struct Graph {
-    pub nodes: Vec<Node>,
+pub struct Graph<T> {
+    pub nodes: Vec<Node<T>>,
     pub edges: Vec<Edge>,
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
-pub struct Node(pub i32);
+pub struct Node<T> (pub T);
 
 #[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
 pub struct Edge(pub i32, pub i32);
 
-impl Graph {
-    pub fn new(nodes: Vec<Node>, edges: Vec<Edge>) -> Self {
+impl<T> Graph<T> {
+    pub fn new(nodes: Vec<Node<T>>, edges: Vec<Edge>) -> Self {
         Graph { nodes, edges }
     }
 }
 
-impl From<i32> for Node {
-    fn from(item: i32) -> Self {
+impl<T> From<T> for Node<T> {
+    fn from(item: T) -> Self {
         Node(item)
     }
 }
 
-impl Node {
-    pub fn value(&self) -> i32 {
+impl<T> Node<T> {
+    pub fn value(&self) -> T 
+    where T: Copy{
         self.0
     }
 
-    pub fn neighbors(&self, graph: &Graph) -> Vec<Node> {
+    pub fn neighbors(&self, graph: &Graph<T>) -> Vec<Node<T>> 
+    where T: PartialEq + Copy + Hash
+    {
         graph
             .nodes
             .iter()
@@ -69,16 +45,18 @@ impl Node {
 
 // --> ADD AND REMOVE NODES
 
-pub fn add_node(graph: Graph, to_add: Node) -> Graph {
+pub fn add_node<T>(graph: Graph<T>, to_add: Node<T>) -> Graph<T> {
     let mut new_vec = graph;
     new_vec.nodes.push(to_add);
     new_vec
 }
 
-pub fn rem_node(graph: Graph, to_remove: Node) -> Graph {
+pub fn rem_node<T>(graph: Graph<T>, to_remove: Node<T>) -> Graph<T> 
+where T: PartialEq
+{
     let mut nodes = graph.nodes;
-    nodes.retain(|value: &Node | *value != to_remove);
-    let new_vec: Graph = Graph {
+    nodes.retain(|value: &Node<T> | *value != to_remove);
+    let new_vec: Graph<T> = Graph {
         nodes: nodes,
         edges: graph.edges,
     };
@@ -87,13 +65,13 @@ pub fn rem_node(graph: Graph, to_remove: Node) -> Graph {
 
 // --> ADD AND REMOVE EDGES
 
-pub fn add_edge(graph: Graph, to_add: Edge) -> Graph {
+pub fn add_edge<T>(graph: Graph<T>, to_add: Edge) -> Graph<T> {
     let mut new_vec = graph;
     new_vec.edges.push(to_add);
     new_vec
 }
 
-pub fn rem_edge(graph: Graph, to_remove: Edge) -> Graph {
+pub fn rem_edge<T>(graph: Graph<T>, to_remove: Edge) -> Graph<T> {
     let mut edges: Vec<Edge> = graph.edges;
     edges.retain(|value: &Edge | *value != to_remove);
     let new_vec = Graph {
@@ -103,62 +81,16 @@ pub fn rem_edge(graph: Graph, to_remove: Edge) -> Graph {
     new_vec
 }
 
-// SERDE INTO TRIVIAL GRAPH FORMAT
+// --> SERDE INTO TRIVIAL GRAPH FORMAT
+// ...
 
-// BREADTH FIRST SEARCH
-// Use a list that stores nodes that need to be browsed.
-// In one iteration of the algorythm:
-// - if the list is not empty, the node is extracted from the list
-// - the extracted node is visited (processed)
-// - all of the children are placed into the list
-// pub fn bfs(graph: GraphType, start_node: i32, end_node: i32) -> Option<Vec<Option<i32>>> {
-//     let mut queue = Queue::new();
-//     queue.enqueue(start_node);
+// --> BREADTH FIRST SEARCH
 
-//     let mut visisted_nodes = vec![false; graph.len()];
-//     visisted_nodes[0] = true;
-
-//     let mut prev: Vec<Option<i32>> = vec![None; graph.len()];
-
-//     // 'allows to break out of outer loop from within
-//     'outer: while !queue.is_empty() {
-//         let current_node = queue.dequeue();
-//         for v in graph[current_node as usize].iter() {
-//             if *v == end_node {
-//                 prev[*v as usize] = Some(current_node);
-//                 break 'outer;
-//             }
-
-//             if !visisted_nodes[*v as usize] {
-//                 queue.enqueue(*v);
-//                 visisted_nodes[*v as usize] = true;
-//                 prev[*v as usize] = Some(current_node);
-//             }
-//         }
-//     }
-
-//     let mut path = Vec::new();
-//     let mut at = Some(end_node);
-//     while at != None {
-//         path.push(at);
-//         at = prev[at.unwrap_or(0) as usize];
-//     }
-
-//     path.reverse();
-
-//     return match path[0] {
-//         Some(x) if x == start_node => Some(path),
-//         _ => None,
-//     };
-// }
-
-fn main() {}
-
-
-pub fn bfs(graph: &Graph, root: Node, target: Node) -> Option<Vec<i32>> {
-    println!("root {:?} target {:?}", root, target);
-    let mut visited: HashSet<Node> = HashSet::new();
-    let mut history: Vec<i32> = Vec::new();
+pub fn bfs<T>(graph: &Graph<T>, root: Node<T>, target: Node<T>) -> Option<Vec<T>> 
+where T: PartialEq + Copy + Hash + Eq {
+    // println!("root {:?} target {:?}", root, target);
+    let mut visited: HashSet<Node<T>> = HashSet::new();
+    let mut history: Vec<T> = Vec::new();
     let mut queue = VecDeque::new();
 
     visited.insert(root);
@@ -167,7 +99,7 @@ pub fn bfs(graph: &Graph, root: Node, target: Node) -> Option<Vec<i32>> {
         history.push(currentnode.value());
 
         if currentnode == target {
-            println!("Goal is found: {:?}", history);
+            // println!("Goal is found: {:?}", history);
             return Some(history);
         }
 
@@ -181,3 +113,5 @@ pub fn bfs(graph: &Graph, root: Node, target: Node) -> Option<Vec<i32>> {
 
     None
 }
+
+fn main() {}
