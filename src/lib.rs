1124bc074e64ba8acb8e17e34e4825907924cbeb
antonovmike <antonvma@outlook.com>
2022-11-09 16:53:17 +0500

    deserialization panic

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use std::{
     hash::Hash, fmt::{Display, Debug}, io::BufRead
 };
 use serde::{Deserialize, Serialize};
+use std::io::Read;
 
 #[derive(Clone)]
 pub struct Graph<T> {
@@ -140,49 +141,53 @@ where T: Serialize + Copy + Display + ToString + std::fmt::Debug {
         .expect("Couldn't open file");
 
     // -> NAMED FIELDS
-    let mut result: BTreeMap<String, String> = BTreeMap::new();
-    let gr_lenght = graph.edges.len();
-    for i in 0..gr_lenght {
-        let key = format!("Edge {}", i);
-        let serialized = serde_yaml::to_string(&into_structure(graph, i))
-            .unwrap().clone().into_bytes();
-        let serialized: Vec<u8> = serialized
-            .into_iter()
-            .take_while(|&x| x != 0)
-            .collect::<Vec<u8>>();
+    // let mut result: BTreeMap<String, String> = BTreeMap::new();
+    // let gr_lenght = graph.edges.len();
+    // for i in 0..gr_lenght {
+    //     let key = format!("Edge {}", i);
+    //     let serialized = serde_yaml::to_string(&into_structure(graph, i))
+    //         .unwrap().clone().into_bytes();
+    //     let serialized: Vec<u8> = serialized
+    //         .into_iter()
+    //         .take_while(|&x| x != 0)
+    //         .collect::<Vec<u8>>();
         
-        let value_serialized = String::from_utf8(serialized).expect("Invalid utf8 message");
+    //     let value_serialized = String::from_utf8(serialized).expect("Invalid utf8 message");
 
-        result.insert(key, value_serialized);
-    }
+    //     result.insert(key, value_serialized);
+    // }
 
-    serde_yaml::to_writer(file, &result).unwrap();
+    // serde_yaml::to_writer(file, &result).unwrap();
 
     // -> SIMPLE NOT NAMED FIELDS
-    // let triv_gr: GraphStructure<T> = GraphStructure { 
-    //     first_node: graph.nodes.clone(), second_node: graph.nodes.clone(), edge: graph.edges.clone() 
-    // };
-    // let serialized = serde_yaml::to_string(&triv_gr).unwrap();
-    // println!("serialized\n{:?}", serialized);
-    // serde_yaml::to_writer(file, &serialized).unwrap();
+    let triv_gr: GraphStructure<T> = GraphStructure { 
+        first_node: graph.nodes.clone(), second_node: graph.nodes.clone(), edge: graph.edges.clone() 
+    };
+    let serialized = serde_yaml::to_string(&triv_gr).unwrap();
+    println!("serialized\n{:?}", serialized);
+    serde_yaml::to_writer(file, &serialized).unwrap();
 }
 
 // RETURNS GraphStructure
-pub fn deserial_triv<T>(path: &str) -> Vec<GraphStructure<T>>
+pub fn deserial_triv<'de, T>(path: &'de str) -> GraphStructure<T>
 // -> Graph<T> 
-where T: Copy + Display + ToString + std::fmt::Debug {
-    let mut all_lines: Vec<String> = vec![];
+where T: Deserialize<'de> + Copy + Display + ToString + std::fmt::Debug {
+    // let file = std::fs::OpenOptions::new()
+    //     .write(true)
+    //     .open(path)
+    //     .expect("Couldn't open file");
+    // let mut all_lines: Vec<String> = vec![];
 
-    for line in std::io::BufReader::new(
-        std::fs::File::open(path).expect("Failed at opening file.")
-    ).lines() {
-        let words = line.unwrap();
-        all_lines.push(words)
-    }
+    // for line in std::io::BufReader::new(
+    //     std::fs::File::open(path).expect("Failed at opening file.")
+    // ).lines() {
+    //     let words = line.unwrap();
+    //     all_lines.push(words)
+    // }
     
-    let mut edge_index = 0;
+    // let mut edge_index = 0;
 
-    let mut vec_of_graphs: Vec<GraphStructure<T>> = vec![];
+    // let mut vec_of_graphs: Vec<GraphStructure<T>> = vec![];
     
     // for i in 0..all_lines.len() {
     //     let edge_index_string = format!("Edge {}: |", edge_index);
@@ -190,16 +195,21 @@ where T: Copy + Display + ToString + std::fmt::Debug {
     //         let each_part = format!(
     //             "{}\n{}\n{}", &all_lines[i + 1], &all_lines[i + 2], &all_lines[i + 3]
     //         );
-    //         let deser: GraphStructure = serde_yaml::from_str(&each_part).unwrap();
+    //         let deser: GraphStructure<T> = serde_yaml::from_str(&each_part).unwrap();
     //         vec_of_graphs.push(deser);
     //         edge_index += 1;
     //     }
     // }
-
+    
+    // to_writer(file, &result).unwrap();
     // let a = &vec_of_graphs[0].first_node.remove(5);
     // let b = *a;
     
-    vec_of_graphs
+    // vec_of_graphs
+
+    let deserialized: GraphStructure<T> = serde_yaml::from_str(path).unwrap();
+    // println!("DESER\n{:?}", deserialized);
+    deserialized
 }
 
 
